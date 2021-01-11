003b5736bb204d77a929925f7e72b3cd4aa3da7a
chav1961 <chav1961@winmain>
2021-01-11 09:40:58 +0500

    Change Pure Library version

--- x.bat
+++
@@ -1,3 +0,0 @@
-set MAVEN_USERNAME=chav1961
-set MAVEN_PASSWORD=Kb12212949
-mvn -X deploy:deploy-file -Dfile=./target/purelib-0.0.4.jar -DpomFile=./pom.xml -DrepositoryId=github -Durl=https://maven.pkg.github.com -Dtoken=0.0.4 -Dregistry=https://maven.pkg.github.com/chav1961 >zzz
