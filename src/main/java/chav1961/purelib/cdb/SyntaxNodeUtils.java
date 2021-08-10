271f5415f67895c02bd96ab73103b99df815206b
chav1961 <chav1961@mail.ru>
2021-08-10 16:15:14 +0500

    Daily commit

--- src/main/java/chav1961/purelib/cdb/SyntaxNodeUtils.java
+++ src/main/java/chav1961/purelib/cdb/SyntaxNodeUtils.java
@@ -1,6 +1,10 @@
 package chav1961.purelib.cdb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import chav1961.purelib.basic.SimpleURLClassLoader;
+import chav1961.purelib.basic.Utils;
 import chav1961.purelib.basic.exceptions.ContentException;
 import chav1961.purelib.cdb.interfaces.LexProcessor;
 import chav1961.purelib.cdb.interfaces.LexemaInterface;
@@ -65,13 +69,141 @@ public class SyntaxNodeUtils {
 			throw new NullPointerException("Node can't be null"); 
 		}
 		else if (callback == null) {
-			throw new NullPointerException("Node can't be null"); 
+			throw new NullPointerException("Node callback can't be null"); 
 		}
 		else {
 			return walkUpInternal(node,callback) != ContinueMode.STOP;
 		}
 	}
+	
+	/**
+	 * <p>Build DNF by source syntax tree. Syntax tree implies keeping operands for OR and AND nodes in the 'children' field and for NOT nodes in the 'cargo' one</p>
+	 * @param <T> node type
+	 * @param <Clazz> node class type
+	 * @param node root node. Can't be null
+	 * @param orNode mark of OR node type. Can't be null
+	 * @param andNode mark of AND node type. Can't be null
+	 * @param notNode  mark of NOT node type. Can't be null
+	 * @param callback callback to process one of the DNF records. Can't be null
+	 * @return true if all DNFs were processed, false otherwise
+	 * @throws NullPointerException on any parameter is null
+	 */
+	public static <T extends Enum<?>,Clazz extends SyntaxNode<T,Clazz>> boolean buildDNF(final SyntaxNode<T,Clazz> node, final T orNode, final T andNode, final T notNode, final WalkCallback<T,Clazz> callback) throws NullPointerException {
+		if (node == null) {
+			throw new NullPointerException("Node to build DNF for can't be null"); 
+		}
+		else if (orNode == null) {
+			throw new NullPointerException("OR node type can't be null"); 
+		}
+		else if (andNode == null) {
+			throw new NullPointerException("AND node type can't be null"); 
+		}
+		else if (notNode == null) {
+			throw new NullPointerException("NOT node type can't be null"); 
+		}
+		else if (callback == null) {
+			throw new NullPointerException("Node callback can't be null"); 
+		}
+		else {
+			final List<DNFStack<T, Clazz>>	stack = new ArrayList<>();
+			ContinueMode					total;
+		
+			firstBuildDNF(node, orNode, andNode, notNode, false, stack);
+			do {final SyntaxNode<T,Clazz>	item = stackToNode(stack, node, andNode);
+				final ContinueMode			enter = callback.process(NodeEnterMode.ENTER, item);
+				final ContinueMode			exit = callback.process(NodeEnterMode.EXIT, item);
+				
+				total = Utils.resolveContinueMode(enter, exit);
+			} while (total == ContinueMode.CONTINUE && nextBuildDNF(node, orNode, andNode, notNode, false, stack));
+			
+			return total == ContinueMode.CONTINUE;
+		}
+	}
 
+	private static <T extends Enum<?>,Clazz extends SyntaxNode<T,Clazz>> void firstBuildDNF(final SyntaxNode<T, Clazz> node, final T orNode, final T andNode, final T notNode, boolean notNodeDetected, final List<DNFStack<T, Clazz>> stack) {
+		if (node.getType() == orNode) {	// OR - take the same first item and store it's number in the stack
+			firstBuildDNF(node.children[0], orNode, andNode, notNode, notNodeDetected, stack);
+			stack.add(new DNFStack<>(true, 0, node));
+		}
+		else if (node.getType() == andNode) {	// AND - place all items into the stack
+			for (int index = 0; index < node.children.length; index++) {
+				firstBuildDNF(node.children[index], orNode, andNode, notNode, notNodeDetected, stack);
+			}
+		}
+		else if (node.getType() == notNode) {	// NOT - swap OR and AND markers and set NOT detection
+			firstBuildDNF((SyntaxNode<T, Clazz>) node.cargo, andNode, orNode, notNode, !notNodeDetected, stack);
+		}
+		else {
+			if (notNodeDetected) {	// Otherwise - place content into stack (possibly with NOT)
+				final SyntaxNode<T, Clazz> not = (SyntaxNode<T, Clazz>) node.clone();
+				
+				not.type = notNode;
+				not.cargo = node;
+				stack.add(new DNFStack<>(false, -1, not));
+			}
+			else {
+				stack.add(new DNFStack<>(false, -1, node));
+			}
+		}
+	}
+
+	private static <T extends Enum<?>,Clazz extends SyntaxNode<T,Clazz>> boolean nextBuildDNF(final SyntaxNode<T, Clazz> node, final T orNode, final T andNode, final T notNode, boolean notNodeDetected, final List<DNFStack<T, Clazz>> stack) {
+		if (node.getType() == orNode) {
+			final DNFStack<T,Clazz>	current = stack.remove(stack.size()-1);
+			
+			if (current.isOrNode) {	// OR - extract OR description form stack and try to build next path
+				if (nextBuildDNF(node.children[current.orIndex], orNode, andNode, notNode, notNodeDetected, stack)) {   // Nest path for the given item successful
+					stack.add(current);
+					return true;
+				}
+				else if (current.orIndex < node.children.length-1) {	// Next path for the given item failed - use next item in OR
+					firstBuildDNF(node.children[++current.orIndex], orNode, andNode, notNode, notNodeDetected, stack);
+					stack.add(current);
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				throw new IllegalArgumentException();
+			}
+		}
+		else if (node.getType() == andNode) {	// AND - pop stack content in inverse order.
+			for (int index = node.children.length - 1; index >= 0; index--) {
+				if (nextBuildDNF(node.children[index], orNode, andNode, notNode, notNodeDetected, stack)) {	// Try to build next path
+					for (int forward = index+1; forward < node.children.length; forward++) {	// Next path successful - process tail of the AND list
+						firstBuildDNF(node.children[forward], orNode, andNode, notNode, notNodeDetected, stack);
+					}						
+					return true;
+				}
+			}
+			return false;
+		}
+		else if (node.getType() == notNode) {	// NOT - swap OR and AND markers and set NOT detection
+			return nextBuildDNF((SyntaxNode<T, Clazz>) node.cargo, andNode, orNode, notNode, !notNodeDetected, stack);
+		}
+		else {	// Otherwise - remove content from stack
+			stack.remove(stack.size()-1);
+			return false;
+		}
+	}
+
+	private static <T extends Enum<?>,Clazz extends SyntaxNode<T,Clazz>> SyntaxNode<T, Clazz> stackToNode(final List<DNFStack<T, Clazz>> stack, final SyntaxNode<T, Clazz> node, final T andNode) {
+		final List<SyntaxNode<T, Clazz>>	children = new ArrayList<>();
+		final SyntaxNode<T, Clazz> 			result = (SyntaxNode<T, Clazz>) node.clone(); 
+		
+		for (DNFStack<T, Clazz> item : stack) {
+			if (!item.isOrNode) {
+				children.add(item.child);
+			}
+		}
+		result.type = andNode;
+		result.children = (Clazz[]) children.toArray(new SyntaxNode[children.size()]);
+		return result;
+	}
+
+	
 	@FunctionalInterface
 	public interface LexProducer<LexType extends Enum<?>> {
 		LexemaInterface<LexType> newLexema(int row, int col, LexType lexType, char[] content, int from, int to);
@@ -133,6 +265,23 @@ loop:			for (Clazz item : node.children) {
 				callback.process(NodeEnterMode.EXIT,node);
 				return ContinueMode.STOP;
 			default: throw new UnsupportedOperationException("Continue node type ["+rc+"] is not supported yet"); 
+		}
+	}
+	
+	private static class DNFStack<T extends Enum<?>,Clazz extends SyntaxNode<T,Clazz>> {
+		boolean					isOrNode;
+		int						orIndex;
+		SyntaxNode<T, Clazz>	child;
+		
+		public DNFStack(final boolean isOrNode, final int orIndex, final SyntaxNode<T, Clazz> child) {
+			this.isOrNode = isOrNode;
+			this.orIndex = orIndex;
+			this.child = child;
+		}
+
+		@Override
+		public String toString() {
+			return "DNFStack [isOrNode=" + isOrNode + ", orIndex=" + orIndex + ", child=" + child + "]";
 		}
 	}
 }
---
+++ src/test/java/chav1961/purelib/cdb/SyntaxNodeUtilsTest.java
@@ -0,0 +1,124 @@
+package chav1961.purelib.cdb;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import chav1961.purelib.enumerations.ContinueMode;
+import chav1961.purelib.enumerations.NodeEnterMode;
+
+public class SyntaxNodeUtilsTest {
+	static enum NodeType {
+		OR, AND, NOT, TERM
+	}
+	
+	@Test
+	public void buildDNFTest() {
+		final int[]		count = new int[1];
+		
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	firstOperand = new SyntaxNode(0, 0, NodeType.TERM, 0, null);
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	secondOperand = new SyntaxNode(0, 0, NodeType.TERM, 1, null);
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	thirdOperand = new SyntaxNode(0, 0, NodeType.TERM, 2, null);
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	forthOperand = new SyntaxNode(0, 0, NodeType.TERM, 3, null);
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	fivthOperand = new SyntaxNode(0, 0, NodeType.TERM, 4, null);
+		
+		count[0] = 0;	// term1
+		SyntaxNodeUtils.buildDNF((SyntaxNode)firstOperand, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				Assert.assertEquals(1, node.children.length);
+				Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(1, count[0]);
+
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	andNode1 = new SyntaxNode(0, 0, NodeType.AND, 0, null, firstOperand, secondOperand);
+		
+		count[0] = 0;	// term1 and term2
+		SyntaxNodeUtils.buildDNF((SyntaxNode)andNode1, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				Assert.assertEquals(2, node.children.length);
+				Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+				Assert.assertEquals(NodeType.TERM, node.children[1].getType());
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(1, count[0]);
+
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	orNode1 = new SyntaxNode(0, 0, NodeType.OR, 0, null, firstOperand, secondOperand);
+		
+		count[0] = 0;	// term1 or term2
+		SyntaxNodeUtils.buildDNF((SyntaxNode)orNode1, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				Assert.assertEquals(1, node.children.length);
+				Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(2, count[0]);
+		
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	andNode2 = new SyntaxNode(0, 0, NodeType.AND, 0, null, thirdOperand, forthOperand);
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	orNode2 = new SyntaxNode(0, 0, NodeType.OR, 0, null, andNode1, andNode2);
+		
+		count[0] = 0;	// term1 and term2 or term3 and term 4
+		SyntaxNodeUtils.buildDNF((SyntaxNode)orNode2, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				Assert.assertEquals(2, node.children.length);
+				Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+				Assert.assertEquals(NodeType.TERM, node.children[1].getType());
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(2, count[0]);
+
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	orNode3 = new SyntaxNode(0, 0, NodeType.OR, 0, null, forthOperand, fivthOperand);
+		andNode2 = new SyntaxNode(0, 0, NodeType.AND, 0, null, thirdOperand, orNode3);
+		orNode2 = new SyntaxNode(0, 0, NodeType.OR, 0, null, andNode1, andNode2);
+	
+		count[0] = 0;	// term1 and term2 or term3 and (term 4 or term5)
+		SyntaxNodeUtils.buildDNF((SyntaxNode)orNode2, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				Assert.assertEquals(2, node.children.length);
+				Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+				Assert.assertEquals(NodeType.TERM, node.children[1].getType());
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(3, count[0]);
+
+		SyntaxNode<NodeType, SyntaxNode<NodeType, SyntaxNode>>	notNode = new SyntaxNode(0, 0, NodeType.NOT, 0, orNode3);
+		andNode2 = new SyntaxNode(0, 0, NodeType.AND, 0, null, thirdOperand, notNode);
+		orNode2 = new SyntaxNode(0, 0, NodeType.OR, 0, null, andNode1, andNode2);
+
+		count[0] = 0;	// term1 and term2 or term3 and not (term 4 or term5)
+		SyntaxNodeUtils.buildDNF((SyntaxNode)orNode2, NodeType.OR, NodeType.AND, NodeType.NOT, (mode, node)->{
+			if (mode == NodeEnterMode.ENTER) {
+				Assert.assertEquals(NodeType.AND, node.getType());
+				if (node.children.length == 2) {
+					Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+					Assert.assertEquals(NodeType.TERM, node.children[1].getType());
+				}
+				else if (node.children.length == 3) {
+					Assert.assertEquals(NodeType.TERM, node.children[0].getType());
+					Assert.assertEquals(NodeType.NOT, node.children[1].getType());
+					Assert.assertEquals(NodeType.NOT, node.children[2].getType());
+				}
+				else {
+					Assert.fail("Error: neither 2 nor 3 for children length");
+				}
+				count[0]++;
+			}
+			return ContinueMode.CONTINUE;
+		});
+		Assert.assertEquals(2, count[0]);
+	}
+}
