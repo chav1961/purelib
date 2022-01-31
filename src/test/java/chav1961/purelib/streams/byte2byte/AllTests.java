3dbb0538e00fbd42b871576cad1091b456aa8444
chav1961 <chav1961@winmain>
2022-01-31 23:09:35 +0500

    Daily commit

--- src/test/java/chav1961/purelib/streams/byte2byte/AllTests.java
+++ src/test/java/chav1961/purelib/streams/byte2byte/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ZLibStreamTest.class , PseudoRandomInputStreamTest.class })
+@SuiteClasses({ ZLibStreamTest.class , PseudoRandomInputStreamTest.class, SqlStreamsTest.class })
 public class AllTests {
 
 }
