bc252a583a23ff8a3fea94d3356a231b96aca9a3
chav1961 <chav1961@winmain>
2022-08-10 23:10:10 +0500

    Daily commit

---
+++ src/main/java/chav1961/purelib/streams/MultipartInputStream.java
@@ -0,0 +1,14 @@
+package chav1961.purelib.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MultipartInputStream extends InputStream {
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/chav1961/purelib/streams/MultipartOutputStream.java
@@ -0,0 +1,12 @@
+package chav1961.purelib.streams;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MultipartOutputStream extends OutputStream {
+	@Override
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
