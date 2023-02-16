c3ff8462062a11dc581a9458b90ff89f8d4f512a
chav1961 <chav1961@mail.ru>
2023-02-16 16:32:58 +0500

    Daily commit

--- src/main/java/chav1961/purelib/streams/interfaces/PrologueEpilogueMaster.java
+++ src/main/java/chav1961/purelib/streams/interfaces/PrologueEpilogueMaster.java
@@ -2,6 +2,17 @@ package chav1961.purelib.streams.interfaces;
 
 import java.io.IOException;
 
+import chav1961.purelib.streams.char2char.CreoleWriter;
+
+
+/**
+ * <p>This interface prepares prologue/epilogue for Creole writer. You can print any html tags here for example</p>
+ * @author Alexander Chernomyrdin aka chav1961
+ * @since 0.0.2
+ * @param <Wr> writer to print content required to
+ * @param <T> writer instance associated with the given content
+ * @see CreoleWriter  
+ */
 @FunctionalInterface
 public interface PrologueEpilogueMaster<Wr,T> {
 	boolean writeContent(Wr writer, T instance) throws IOException;
