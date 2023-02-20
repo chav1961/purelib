1414d45046eb6821863eba1d1dd339f0acde74b3
chav1961 <chav1961@winmain>
2023-02-20 23:34:17 +0500

    Daily commit

--- src/main/java/chav1961/purelib/ui/swing/useful/JSimpleSplash.java
+++ src/main/java/chav1961/purelib/ui/swing/useful/JSimpleSplash.java
@@ -6,36 +6,44 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.SplashScreen;
-import java.io.Closeable;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import chav1961.purelib.basic.exceptions.EnvironmentException;
 import chav1961.purelib.basic.interfaces.ProgressIndicator;
 
+/**
+ * <p>This class is a splash screen manager. It can show splash screen before main GUI application will be appeared and supports
+ * {@linkplain ProgressIndicator} interface to refresh application preparation. To correct working, include <b>SplashScreen-Image</b>
+ * option into your manifest.mf file</p> 
+ * @author Alexander Chernomyrdin aka chav1961
+ * @since 0.0.6
+ */
 public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 	private static final int	STRING_X_GAP = 20;
 	private static final int	STRING_Y_GAP = 25;
-	private static final Color	STRING_COLOR = Color.BLACK;
+	private static final Color	STRING_COLOR = Color.WHITE;
 	private static final Font	STRING_FONT = new Font("Courier", Font.PLAIN, 12);
 	private static final int	RECT_X_GAP = 20;
 	private static final int	RECT_Y_GAP = 5;
 	private static final int	RECT_HEIGHT = 10;
 	private static final float	RECT_PERCENT = 0.02f;
 	private static final Color	RECT_BOUND_COLOR = Color.BLACK;
-	private static final Color	RECT_COMPLETED_COLOR = Color.CYAN;
+	private static final Color	RECT_COMPLETED_COLOR = Color.BLUE;
 	private static final Color	RECT_RETAINED_COLOR = Color.WHITE;
 	
 	private final SplashScreen 	splash;
 	private final boolean		available;
 	private final Graphics2D	g2d;
+	private Color				boundColor = RECT_BOUND_COLOR, completedColor = RECT_COMPLETED_COLOR, retainedColor = RECT_RETAINED_COLOR, stringColor = STRING_COLOR;
 	private boolean				needDraw = false, needRect = false, needDrawStage = false, needRectStage = false;
 	private String				caption, subcaption;
 	private long				old, current, total, discrete;
 	private long				oldStage, currentStage, totalStage, discreteStage;
 
-	public JSimpleSplash() throws NullPointerException, EnvironmentException {
+	/**
+	 * <p>Show simple splash with default splash screen</p>
+	 */
+	public JSimpleSplash() {
 		boolean			available = false;
 		Graphics2D		g2d = null;
 		SplashScreen	splash = null;
@@ -58,8 +66,14 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 		this.available = available;
 		this.g2d = g2d;
 	}
-	
-	public JSimpleSplash(final URL imageURL) throws IOException, NullPointerException, EnvironmentException {
+
+	/**
+	 * <p>Show simple splash with custom splash URL</p>
+	 * @param imageURL custom splash url.
+	 * @throws IOException on any I/O errors
+	 * @throws NullPointerException custom URI is null
+	 */
+	public JSimpleSplash(final URL imageURL) throws IOException, NullPointerException {
 		this.splash = SplashScreen.getSplashScreen();
 		
 		if (imageURL == null) {
@@ -79,7 +93,7 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 	@Override
 	public void close() throws RuntimeException {
 		end();
-		if (available) {
+		if (available && splash.isVisible()) {
 			splash.close();
 		}
 	}
@@ -217,11 +231,99 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 		}
 	}
 
+	/**
+	 * <p>Get foreground (string caption) color</p>
+	 * @return foreground color. Can't be null
+	 */
+	public Color getForegroundColor() {
+		return stringColor;
+	}
+	
+	/**
+	 * <p>Get background (progress background) color</p>
+	 * @return background color. Can't be null
+	 */
+	public Color getBackgroundColor() {
+		return retainedColor;
+	}
+	
+	/**
+	 * <p>Get processed (progress processed) color</p>
+	 * @return progress processed color. Can't be null
+	 */
+	public Color getForegroundProgressColor() {
+		return completedColor;
+	}
+	
+	/**
+	 * <p>Get border (progress border) color</p>
+	 * @return progress border color.Can't be null
+	 */
+	public Color getBorderColor() {
+		return boundColor;
+	}
+
+	/**
+	 * <p>Set foreground (string) color.</p>
+	 * @param color color to set. Can't be null
+	 * @throws NullPointerException color to set is null
+	 */
+	public void setForegroundColor(final Color color) throws NullPointerException {
+		if (color == null) {
+			throw new NullPointerException("Color to set can't be null");
+		}
+		else {
+			stringColor = color;
+		}
+	}
+	
+	/**
+	 * <p>Set foreground (progress background) color.</p>
+	 * @param color color to set. Can't be null
+	 * @throws NullPointerException color to set is null
+	 */
+	public void setBackgroundColor(final Color color) throws NullPointerException  {
+		if (color == null) {
+			throw new NullPointerException("Color to set can't be null");
+		}
+		else {
+			retainedColor = color;
+		}
+	}
+	
+	/**
+	 * <p>Set processed (progress processed) color.</p>
+	 * @param color color to set. Can't be null
+	 * @throws NullPointerException color to set is null
+	 */
+	public void setForegroundProgressColor(final Color color) throws NullPointerException  {
+		if (color == null) {
+			throw new NullPointerException("Color to set can't be null");
+		}
+		else {
+			completedColor = color;
+		}
+	}
+	
+	/**
+	 * <p>Set border (progress border) color.</p>
+	 * @param color color to set. Can't be null
+	 * @throws NullPointerException color to set is null
+	 */
+	public void setBorderColor(final Color color) throws NullPointerException  {
+		if (color == null) {
+			throw new NullPointerException("Color to set can't be null");
+		}
+		else {
+			boundColor = color;
+		}
+	}
+	
 	protected void refresh() {
 		if (available) {
 			try {
-//				redraw(g2d);
-//				splash.update();
+				redraw(g2d);
+				splash.update();
 			} catch (IllegalStateException exc) {
 			}
 		}
@@ -264,27 +366,27 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 		        	
 		        	drawRect(g2d,yGap,height,current,total);
 		        	yGap += height;
-//		        	final long	curr = current;
-//		        	final int	width = rect.width - 2 * RECT_X_GAP;
-//		        	final int	percent = (int) (100.0 * curr / total);
-//		        	final int	widthLeft = width * percent / 100; 
-//		        	final int	widthRight = width - widthLeft;
-//		        	
-//			        g2d.setColor(RECT_COMPLETED_COLOR);
-//			        g2d.fillRect(RECT_X_GAP, rect.height - RECT_Y_GAP - height, widthLeft, height);
-//			        g2d.setColor(RECT_RETAINED_COLOR);
-//			        g2d.fillRect(RECT_X_GAP + widthLeft, rect.height - RECT_Y_GAP - height, widthRight, height);
-//			        g2d.setColor(RECT_BOUND_COLOR);
-//			        g2d.drawRect(RECT_X_GAP, rect.height - RECT_Y_GAP - height, width, height);
+		        	final long	curr = current;
+		        	final int	width = rect.width - 2 * RECT_X_GAP;
+		        	final int	percent = (int) (100.0 * curr / total);
+		        	final int	widthLeft = width * percent / 100; 
+		        	final int	widthRight = width - widthLeft;
+		        	
+			        g2d.setColor(completedColor);
+			        g2d.fillRect(RECT_X_GAP, rect.height - RECT_Y_GAP - height, widthLeft, height);
+			        g2d.setColor(retainedColor);
+			        g2d.fillRect(RECT_X_GAP + widthLeft, rect.height - RECT_Y_GAP - height, widthRight, height);
+			        g2d.setColor(boundColor);
+			        g2d.drawRect(RECT_X_GAP, rect.height - RECT_Y_GAP - height, width, height);
 		        }
 		        if (cap != null) {
 		        	drawText(g2d, yGap, cap);
-//		        	final Font	oldFont = g2d.getFont();
-//		        	
-//			        g2d.setColor(STRING_COLOR);
-//		        	g2d.setFont(STRING_FONT);
-//		        	g2d.drawString(cap, STRING_X_GAP, rect.height - STRING_Y_GAP - (needRect ? height + RECT_Y_GAP : 0));
-//		        	g2d.setFont(oldFont);
+		        	final Font	oldFont = g2d.getFont();
+		        	
+			        g2d.setColor(stringColor);
+		        	g2d.setFont(STRING_FONT);
+		        	g2d.drawString(cap, STRING_X_GAP, rect.height - STRING_Y_GAP - (needRect ? height + RECT_Y_GAP : 0));
+		        	g2d.setFont(oldFont);
 		        }
 	        }
 	        g2d.setColor(oldColor);
@@ -295,7 +397,7 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
 		final Rectangle	rect = splash.getBounds();
     	final Font		oldFont = g2d.getFont();
     	
-        g2d.setColor(STRING_COLOR);
+        g2d.setColor(stringColor);
     	g2d.setFont(STRING_FONT);
     	g2d.drawString(text, STRING_X_GAP, rect.height - STRING_Y_GAP - gap);
     	g2d.setFont(oldFont);
@@ -308,11 +410,11 @@ public class JSimpleSplash implements ProgressIndicator, AutoCloseable {
     	final int		widthLeft = width * percent / 100; 
     	final int		widthRight = width - widthLeft;
     	
-        g2d.setColor(RECT_COMPLETED_COLOR);
+        g2d.setColor(completedColor);
         g2d.fillRect(RECT_X_GAP, rect.height - gap - height, widthLeft, height);
-        g2d.setColor(RECT_RETAINED_COLOR);
+        g2d.setColor(retainedColor);
         g2d.fillRect(RECT_X_GAP + widthLeft, rect.height - gap - height, widthRight, height);
-        g2d.setColor(RECT_BOUND_COLOR);
+        g2d.setColor(boundColor);
         g2d.drawRect(RECT_X_GAP, rect.height - gap - height, width, height);
 	}
 	
