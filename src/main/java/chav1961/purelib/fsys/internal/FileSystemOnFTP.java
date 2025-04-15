934ef530c48ce9f05bf9773420ea722d20241bff
chav1961 <chav1961@mail.ru>
2025-04-15 16:34:48 +0500

    Daily commit

--- src/main/java/chav1961/purelib/fsys/internal/FileSystemOnFTP.java
+++ src/main/java/chav1961/purelib/fsys/internal/FileSystemOnFTP.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.Icon;
@@ -37,27 +39,118 @@ import chav1961.purelib.fsys.interfaces.FileSystemInterfaceDescriptor;
 import chav1961.purelib.i18n.internal.PureLibLocalizer;
 
 public class FileSystemOnFTP extends AbstractFileSystem implements FileSystemInterfaceDescriptor {
-	private static final URI	SERVE = URI.create(FileSystemInterface.FILESYSTEM_URI_SCHEME+":ftp:/");
-	private static final URI	SERVE_S = URI.create(FileSystemInterface.FILESYSTEM_URI_SCHEME+":ftp:/");
-	private static final String	DESCRIPTION = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_DESCRIPTION_SUFFIX;
-	private static final String	VENDOR = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_VENDOR_SUFFIX;
-	private static final String	LICENSE = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_SUFFIX;
-	private static final String	LICENSE_CONTENT = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_CONTENT_SUFFIX;
-	private static final String	HELP = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_HELP_SUFFIX;
-	private static final Icon	ICON = new ImageIcon(FileSystemOnXMLReadOnly.class.getResource("xmlIcon.png"));
+	private static final URI		SERVE = URI.create(FileSystemInterface.FILESYSTEM_URI_SCHEME+":ftp:/");
+	private static final URI		SERVE_S = URI.create(FileSystemInterface.FILESYSTEM_URI_SCHEME+":ftp:/");
+	private static final String		DESCRIPTION = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_DESCRIPTION_SUFFIX;
+	private static final String		VENDOR = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_VENDOR_SUFFIX;
+	private static final String		LICENSE = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_SUFFIX;
+	private static final String		LICENSE_CONTENT = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_CONTENT_SUFFIX;
+	private static final String		HELP = FileSystemFactory.FILESYSTEM_LOCALIZATION_PREFIX+'.'+FileSystemOnFTP.class.getSimpleName()+'.'+FileSystemFactory.FILESYSTEM_LICENSE_HELP_SUFFIX;
+	private static final Icon		ICON = new ImageIcon(FileSystemOnXMLReadOnly.class.getResource("xmlIcon.png"));
+	private static final int[]		DUMMY = new int[0];
 
+	private static enum ResponseType {
+		SUCCESS(true),
+		FAILED(false),
+		IGNORED(true),
+		STARTED(true),
+		COMPLETED(true),
+		UNKNOWN(false),
+		;
+		
+		private final boolean	successful;
+		
+		private ResponseType(final boolean successful) {
+			this.successful = successful;
+		}
+		
+		public boolean isSuccessful() {
+			return successful;
+		}
+	}
+	
 	private static enum Command {
-		UNKNOWN,
-		USER,
-		PASS,
-		CWD,
-		MKD,
-		STOR,
-		RNFR,
-		RNTO,
-		DELE,
-		NLST,
-		STAT,
+		UNKNOWN((R,S)->null, DUMMY, DUMMY),
+		USER((R,S)->null, new int[]{331, 332}, new int[] {421, 500, 501, 530}),
+		PASS((R,S)->null, new int[]{230}, new int[] {332, 421, 500, 501, 503, 530}, new int[] {202}),
+		CWD((R,S)->null, new int[]{250}, new int[] {421, 500, 501, 502, 530, 550}),
+		MKD((R,S)->null, new int[]{257}, new int[] {421, 500, 501, 502, 530, 550}),
+		STOR((R,S)->null, new int[]{125, 150}, new int[]{226, 250}, new int[] {421, 425, 426, 450, 451, 452, 500, 501, 530, 532, 534, 535, 551, 552, 553}, DUMMY),
+		RNFR((R,S)->null, new int[]{350}, new int[] {421, 450, 500, 501, 502, 530, 550}),
+		RNTO((R,S)->null, new int[]{250}, new int[] {421, 500, 501, 502, 503, 530, 532, 553}),
+		DELE((R,S)->null, new int[]{250}, new int[] {421, 450, 500, 501, 502, 530, 550}),
+		NLST((R,S)->null, new int[]{125, 150}, new int[]{226, 250}, new int[] {421, 425, 426, 450, 451, 500, 501, 502, 504, 530, 534, 535, 550}, DUMMY),
+		STAT((R,S)->null, new int[]{212, 213}, new int[] {421, 450, 500, 501, 502, 530}, new int[] {211}),
+		;
+		
+		private final BiFunction<Response, String, Object> callback; 
+		private final int[] 	success;
+		private final int[] 	failed;
+		private final int[] 	ignored;
+		private final int[] 	start;
+		private final int[] 	stop;
+		private final boolean	startStop;	
+		
+		private Command(final BiFunction<Response, String, Object> callback, final int[] success, final int[] failed) {
+			this(callback, success, failed, DUMMY);
+		}
+		
+		private Command(final BiFunction<Response, String, Object> callback, final int[] success, final int[] failed, final int[] ignored) {
+			this.callback = callback;
+			this.success = success;
+			this.failed = failed;
+			this.ignored = ignored;
+			this.start = DUMMY;
+			this.stop = DUMMY;
+			this.startStop = false;
+		}
+
+		private Command(final BiFunction<Response, String, Object> callback, final int[] start, final int[] stop, final int[] failed, final int[] ignored) {
+			this.callback = callback;
+			this.success = DUMMY;
+			this.failed = failed;
+			this.ignored = ignored;
+			this.start = start;
+			this.stop = stop;
+			this.startStop = true;
+		}
+
+		public BiFunction<Response, String, Object> getCallback() {
+			return callback;
+		}
+		
+		public ResponseType getResponseType(final int responseCode) {
+			for(int item : success) {
+				if (item == responseCode) {
+					return ResponseType.SUCCESS;
+				}
+			}
+			for(int item : failed) {
+				if (item == responseCode) {
+					return ResponseType.FAILED;
+				}
+			}
+			for(int item : ignored) {
+				if (item == responseCode) {
+					return ResponseType.IGNORED;
+				}
+			}
+			for(int item : start) {
+				if (item == responseCode) {
+					return ResponseType.STARTED;
+				}
+			}
+			for(int item : stop) {
+				if (item == responseCode) {
+					return ResponseType.COMPLETED;
+				}
+			}
+			return ResponseType.UNKNOWN;
+		}
+		
+		public boolean isStartStopCommand() {
+			return startStop;
+		}
 	}
 	
 	private final FileSystemOnFTP	parent;
@@ -108,6 +201,7 @@ public class FileSystemOnFTP extends AbstractFileSystem implements FileSystemInt
 					throw new IOException(r1.getResponseString());
 				}
 			} catch (InterruptedException exc) {
+				this.server.close();
 				Thread.currentThread().interrupt();
 				throw new IOException(exc);
 			}
@@ -247,67 +341,167 @@ public class FileSystemOnFTP extends AbstractFileSystem implements FileSystemInt
 
 	private Console lockConsole() throws InterruptedException {
 		lock.lockInterruptibly();
-		return new Console() {
-
-			@Override
-			public void close() throws IOException {
-				lock.unlock();
-			}
-
-			@Override
-			public Response sendCommand(final Command command, final Object... parameters) throws IOException {
-				final String 	message = String.format(FILESYSTEM_URI_SCHEME, parameters);
-				serverWr.write(message);
-				serverWr.flush();
-				return buildResponse(serverBRdr.readLine(), command);
-			}
-
-		};
+		return new Console(lock);
 	}
 
-	private Response buildResponse(final String answer, final Command command) {
-		// TODO Auto-generated method stub
-		return new Response() {
-			@Override public Command getCommand() {return command;}
-			@Override public String getResponseString() {return answer;}
-
-			@Override
-			public <T> T getResponseEntity() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public boolean isSuccessful() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public InputStream getInputStream() throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public OutputStream getOutputStream() throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		};
+	private Response buildResponse(final String answer, final Command command, final Console console) throws IOException {
+		if (command.isStartStopCommand()) {
+			return new Response(command, answer, console.sock);
+		}
+		else {
+			return new Response(command, answer);
+		}
 	}
 	
-	private static interface Console extends Closeable {
-		Response sendCommand(final Command command, final Object... parameters) throws IOException;
+	private class Console implements Closeable {
+		private final Lock	lock;
+		private Socket		sock = null;
+		
+		private Console(final Lock lock) {
+			this.lock = lock;
+		}
+		
+		Response sendCommand(final Command command, final Object... parameters) throws IOException {
+			final String 	message = String.format(FILESYSTEM_URI_SCHEME, parameters);
+			serverWr.write(message);
+			serverWr.flush();
+			return buildResponse(serverBRdr.readLine(), command, this);
+		}
+
+		@Override
+		public void close() throws IOException {
+			lock.unlock();
+		}
 	}
 
-	private static interface Response {
-		Command getCommand();
-		String getResponseString();
-		<T> T getResponseEntity();
-		boolean isSuccessful();
-		InputStream getInputStream() throws IOException;
-		OutputStream getOutputStream() throws IOException;
+	private static class Response {
+		private static final Pattern	ANSWER = Pattern.compile("(\\d{3,3})\\s+(.*)");
+		
+		private final Command	command;
+		private final int		responseCode;
+		private final String	responseString;
+		private final Object	responseEntity;
+		
+		private Response(final Command command, final String responseString) throws IOException {
+			final Matcher	m = ANSWER.matcher(responseString);
+			
+			if (!m.matches()) {
+				throw new IOException("Invalid response from FTP server ["+responseString+"]");
+			}
+			else {
+				this.command = command;
+				this.responseCode = Integer.valueOf(m.group(1));
+				this.responseString = m.group(2);
+				this.responseEntity = command.getCallback().apply(this, responseString);
+			}
+		}
+
+		private Response(final Command command, final String responseString, final Socket socket) throws IOException {
+			final Matcher	m = ANSWER.matcher(responseString);
+			
+			if (!m.matches()) {
+				throw new IOException("Invalid response from FTP server ["+responseString+"]");
+			}
+			else {
+				this.command = command;
+				this.responseCode = Integer.valueOf(m.group(1));
+				this.responseString = m.group(2);
+				this.responseEntity = socket;
+			}
+		}
+		
+		Command getCommand() {
+			return command;
+		}
+		
+		int getResponseCode() {
+			return responseCode;
+		}
+		
+		String getResponseString() {
+			return responseString;
+		}
+		
+		ResponseType getResponseType() {
+			return getCommand().getResponseType(getResponseCode());
+		}
+		
+		boolean isSuccessful() {
+			return getResponseType().isSuccessful();
+		}
+		
+		<T> T getResponseEntity() {
+			return (T)responseEntity;
+		}
+		
+		InputStream getInputStream() throws IOException {
+			if (getResponseEntity() instanceof Socket) {
+				final Socket		sock = getResponseEntity();
+				final InputStream	is = sock.getInputStream(); 
+				
+				sock.getOutputStream().close();
+				return new InputStream() {
+					@Override
+					public int read() throws IOException {
+						return is.read();
+					}
+					
+					@Override
+					public int read(byte[] b) throws IOException {
+						return is.read(b);
+					}
+					
+					@Override
+					public int read(byte[] b, int off, int len) throws IOException {
+						return is.read(b, off, len);
+					}
+					
+					@Override
+					public void close() throws IOException {
+						super.close();
+						sock.close();
+					}
+				};
+			}
+			else {
+				throw new IOException("Command doesn't support input stream");
+			}
+		}
+		
+		OutputStream getOutputStream() throws IOException {
+			if (getResponseEntity() instanceof Socket) {
+				final Socket		sock = getResponseEntity();
+				final OutputStream	os = sock.getOutputStream(); 
+				
+				sock.getInputStream().close();
+				return new OutputStream() {
+					@Override
+					public void write(int b) throws IOException {
+						os.write(b);
+					}
+
+					@Override
+					public void write(byte[] b) throws IOException {
+						os.write(b);
+					}
+					
+					@Override
+					public void write(byte[] b, int off, int len) throws IOException {
+						os.write(b, off, len);
+					}
+					
+					@Override
+					public void close() throws IOException {
+						super.close();
+						sock.close();
+					}
+
+				};
+			}
+			else {
+				throw new IOException("Command doesn't support input stream");
+			}
+		}
 	}
 
 	private class FTPDataWrapper implements DataWrapperInterface {
@@ -516,5 +710,5 @@ public class FileSystemOnFTP extends AbstractFileSystem implements FileSystemInt
 		private String getParenPath() {
 			return null;
 		}
-	}	
+	}
 }
